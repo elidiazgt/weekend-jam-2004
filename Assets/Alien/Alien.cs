b4a036b05cc03309dc796bdb71beddffd7d7c10b
Christian Lehnhoff <ChristianLe87@users.noreply.github.com>
2020-02-01 12:08:54 -0600

    Add basic test djfklas

---
+++ Assets/Alien/Alien.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alien : MonoBehaviour
+{
+    public GameObject proyectil;
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Test_Mover();
+        if (Input.GetKeyDown("p"))
+        {
+            Test_Disparar();
+        }
+
+        
+    }
+
+    void Test_Mover()
+    {
+        Vector3 newPosition = new Vector3(
+           /* X */ Input.GetAxis("Horizontal") * 5 * Time.deltaTime,
+           /* Y */ Input.GetAxis("Vertical") * 5 * Time.deltaTime,
+           /* Z */ 0f);
+
+        transform.Translate(newPosition);
+    }
+
+    void Test_Disparar()
+    {
+        GameObject.Instantiate(proyectil, transform.position, transform.rotation);
+    }
+}
---
+++ Assets/Edificio/Edificio.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edificio : MonoBehaviour
+{
+    GameObject imagen1_Casa_100;
+    GameObject imagen2_Casa_50;
+    GameObject imagen3_Casa_0;
+
+    public int vida;
+
+    void Start()
+    {
+        vida = 100;
+
+        imagen1_Casa_100 = GameObject.Find("Edificio_1_300x300");
+        imagen2_Casa_50 = GameObject.Find("Edificio_2_300x300");
+        imagen3_Casa_0 = GameObject.Find("Edificio_3_300x300");
+
+        imagen1_Casa_100.SetActive(false);
+        imagen2_Casa_50.SetActive(false);
+        imagen3_Casa_0.SetActive(false);
+
+    }
+
+
+    void Update()
+    {
+
+        // vida entre 0 y 10
+        if (vida < 10)
+        {
+            imagen1_Casa_100.SetActive(false);
+            imagen2_Casa_50.SetActive(false);
+            imagen3_Casa_0.SetActive(true);
+        }
+
+        // vida entre 10 y 90
+        else if ((vida >=10) && (vida <= 90))
+        {
+            imagen1_Casa_100.SetActive(false);
+            imagen2_Casa_50.SetActive(true);
+            imagen3_Casa_0.SetActive(false);
+        }
+
+        // vida entre 90 y 100
+        else if (vida > 90)
+        {
+            imagen1_Casa_100.SetActive(true);
+            imagen2_Casa_50.SetActive(false);
+            imagen3_Casa_0.SetActive(false);
+        }
+
+    }
+}
