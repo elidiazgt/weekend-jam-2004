8bc88b4991569f09ea6f3e5f43252f04f4fd78ef
Christian Lehnhoff <ChristianLe87@users.noreply.github.com>
2020-02-01 15:02:00 -0600

    Copy things from Christian

---
+++ Assets/Proyectil/Proyectil.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proyectil : MonoBehaviour
+{
+    float timeCount = 0f;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        timeCount += Time.deltaTime;
+
+        // despues de 1.5 segundos de vida, destruir proyectil
+        if (timeCount> 1.5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((collision.name.Contains("Proyectil") == false))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
