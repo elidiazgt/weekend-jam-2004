e73890b739652ba01bccd6d245bb2f3c62738559
devciclolabs <dev@ciclolabs.com>
2020-02-01 12:47:33 -0600

    Merge complete

--- Assets/scripts/MainController.cs
+++ Assets/scripts/MainController.cs
@@ -33,8 +33,7 @@ public class MainController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        notifications = new Notifications();
-    }
+     }
 
     // Update is called once per frame
     void Update()
